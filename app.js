f8f4c5cdec91c42193d667ff02d4203a16d99de8
Facu-navarro <facunava710@gmail.com>
2020-06-06 18:29:26 -0300

    Static Dir

--- app.js
+++ app.js
@@ -19,7 +19,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use('*/img', express.static(path.join(__dirname, 'public/img')));
+app.use('*/javascript', express.static(path.join(__dirname, 'public/javascript')));
+app.use('*/stylesheets', express.static(path.join(__dirname, 'public/stylesheets')));
 app.use(methodOverride('_method'));
 
 
