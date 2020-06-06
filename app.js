3ddd713ba807e048c13da9915a6ce1f3c3d96b8d
gabyacg29 <g.a.cinquemani@gmail.com>
2020-06-06 14:49:30 -0300

    commit

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
+
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
