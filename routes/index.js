165ca8fc6c23ee98b27be685d4bc3af3e3cf41a6
Facu-navarro <facunava710@gmail.com>
2020-06-07 12:49:28 -0300

    Update ruta automatica

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let productos = [];
-  res.render('formProducto', { productos: productos });
+  res.render('index', { productos: productos });
 });
 
 module.exports = router;
